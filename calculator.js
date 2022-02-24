bec9e7c2ff72127d38b5586b803d0828ca002b86
olusegun caleb <olupeniel2000@gmail.com>
2022-02-24 11:44:14 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,40 @@
+const calculator = (num1, num2, operation) => {
+  if (operation instanceof Function) {
+    return operation(num1, num2);
+  }
+  return;
+};
+
+// addition
+const add = (num1, num2) => num1 + num2;
+
+// substraction
+const subtract = (num1, num2) => num1 - num2;
+
+//division
+const division = (num1, num2) => {
+  if (num2 === 0) {
+    return `Error enter denominator greater than 0`;
+  }
+  return num1 / num2;
+};
+
+//exponential
+const exponential = (num1, num2)  => num1 ** num2
+
+const remainder = (num1, num2) => num1 % num2;
+
+let resultAdd = calculator(7, 1, add);
+console.log(resultAdd);
+
+let resultSub = calculator(6, 5, subtract);
+console.log(resultSub);
+
+let resultDiv = calculator(1, 0, division);
+console.log(resultDiv);
+
+let resultRemainder = calculator(3, 2, remainder);
+console.log(resultRemainder);
+
+let resultExpo = calculator(2, 1, exponential);
+console.log(resultExpo);
---
+++ variables.js
@@ -0,0 +1,5 @@
+function fortuneTeller(language, location) {
+return `i can speak ${language} fluently and i also live in ${location}`    
+}
+let result = fortuneTeller("korea", "south korea")
+console.log(result);
