65496b26ba9ce43bbb97ba3b52559df902c3e099
olusegun caleb <olupeniel2000@gmail.com>
2022-02-25 15:34:02 +0100

    third commit

--- learn.js
+++ learn.js
@@ -2,4 +2,10 @@ let num1 = 20
 let num2 = 30 
 let result = num2 - num1
 console.log('answer:' + result);
-  
+
+function sayHello(name) {
+    console.log('hello ' + name + '!');
+}
+sayHello('caleb')
+sayHello('josh')
+sayHello('esther')
