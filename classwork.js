d60b6328255aabb01fcb8840d7312ff5206e6611
olusegun caleb <olupeniel2000@gmail.com>
2022-02-25 14:13:19 +0100

    second commit

--- classwork.js
+++ classwork.js
@@ -10,5 +10,5 @@ function printEvens(array) {
     return newArray
 }
 //return newArray
-    const theArr = [[1,2,3],[4,5,6],[7,8],[9,10,11,12],[13,14,15]]
+    const theArr = [[1,2,3],[4,5,6],[7,8],[9,10,11,12]]
     console.log(printEvens(theArr));
---
+++ learn.js
@@ -0,0 +1,5 @@
+let num1 = 20
+let num2 = 30 
+let result = num2 - num1
+console.log('answer:' + result);
+  
