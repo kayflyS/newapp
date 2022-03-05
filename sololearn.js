6be6d5079558e93393923078f45739317355446a
olusegun caleb <olupeniel2000@gmail.com>
2022-03-05 19:32:35 +0100

    sixth commit

---
+++ sololearn.js
@@ -0,0 +1,18 @@
+//var x = 100
+//console.log(x);
+/*var sayHello = 'hello world! \' i am a javascript programmer.\' ';
+console.log(sayHello);*/
+
+//function main() {
+    //var distance = parseInt(readLine(), 10);
+    //your code goes here
+  /* const speed = 40;
+    const time = distance/speed;
+    const timeInMinutes = time * 60;
+    console.log(timeInMinutes);
+}*/
+
+//let num1 = 7;
+//let num2 = 8;
+//if (num1 < num2) {console.log('WHAT THE FUCK');   
+//}
