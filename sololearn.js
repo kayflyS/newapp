86075244d5c66465f554c9a6a34e469040ed3002
olusegun caleb <olupeniel2000@gmail.com>
2022-03-08 18:40:38 +0100

    newone

--- sololearn.js
+++ sololearn.js
@@ -12,10 +12,23 @@ console.log(sayHello);*/
     console.log(timeInMinutes);
 }*/
 
-let num1 = 7;
-let num2 = 8;
-if (num1 > num2) {console.log('WHAT THE FUCK'); }  
-else {console.log("mango");
+//let num1 = 7;
+//let num2 = 8;
+//if (num1 > num2) {console.log//('WHAT THE FUCK'); }  
+//else {console.log("mango");
 
+//}
+var day = 2;
+switch (day) {
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("Wednesday");
+        break;
+    default:
+        document.write("Another day");
 }
-
