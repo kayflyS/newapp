2a08ea5ccb863e0a29fabff579f5dc4f2a79b27a
olusegun caleb <olupeniel2000@gmail.com>
2022-03-09 20:44:20 +0100

    now

--- hacker.js
+++ hacker.js
@@ -1,7 +1,17 @@
-console.log("Hello, World!");
 
-function greeting(parameterVariable) {
-   return `${parameterVariable}`
-}
-let result = greeting("Welcome to 10 Days of JavaScript!")
-console.log(result);
+    const firstInteger = 4;
+    const secondInteger = 12
+    
+    const firstDecimal = 4.0;
+    const secondDecimal= 4.32
+   
+    const firstString = 'HackerRank ';
+    const secondString = "is the best place to learn and practice coding"
+
+    
+    console.log(firstInteger + Number(secondInteger));
+    
+       console.log(firstDecimal + Number(secondDecimal));
+       console.log(firstString + secondString);
+    
+    
