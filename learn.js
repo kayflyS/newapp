84c88d6232d27d46ff42cf2f3262d42ffabba2f2
olusegun caleb <olupeniel2000@gmail.com>
2022-03-11 13:09:48 +0100

    done

---
+++ learn.js
@@ -0,0 +1,35 @@
+/*function myTax(amount) {
+    let newAmount= amount * 2
+    return newAmount;
+
+    
+}
+let result = myTax(10)
+console.log(result);*/
+    
+/*setTimeout(function () {
+    console.log('now');
+    
+    },5000); */
+
+    /*let counter = 0;
+    function timeout() { 
+        setTimeout(function () {
+            console.log('HI ' + counter++);
+
+            
+        }, 2000)
+    }
+    timeout();*/
+    // DECISION STATEMENTS
+    if (6 > 10){
+        console.log('BADDEST BOY');
+    } else if
+    (7 > 3){
+        console.log('hello world');
+    }
+
+    
+        else{
+           console.log('FLOWERS');
+       }
