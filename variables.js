b80ea1e91aebfdd7c5155066ec82d3389b4bc641
Caleb <olupeniel2000@gmail.com>
2022-03-25 13:59:34 -0700

    correction

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
 function fortuneTeller(language, location, buy) {
 return `i can speak ${language} fluently and i also live in ${location} eating ${buy}`    
     }
-    let result = fortuneTeller("korea", "south korea","mango")
-    console.log(result);
+    let me = fortuneTeller("korea", "south korea","mango")
+    console.log(me);
