ef742ab139176f8c0a2ad01dd94a3af5e426ab16
Caleb <olupeniel2000@gmail.com>
2022-03-25 13:30:28 -0700

    test commit

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
-function fortuneTeller(language, location) {
-return `i can speak ${language} fluently and i also live in ${location}`    
-}
-let result = fortuneTeller("korea", "south korea")
-console.log(result);
+function fortuneTeller(language, location, buy) {
+return `i can speak ${language} fluently and i also live in ${location} eating ${buy}`    
+    }
+    let result = fortuneTeller("korea", "south korea","mango")
+    console.log(result);
