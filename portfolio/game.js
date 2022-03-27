b7d84fd415c34707fa24a9ca4e09a42ccb81fdac
Caleb <olupeniel2000@gmail.com>
2022-03-27 19:00:16 +0100

    updated rps2

--- portfolio/game.js
+++ portfolio/game.js
@@ -4,7 +4,30 @@ const resultD = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice;
 
+let computerchoice;
+
 possibleChoices.forEach(possibleChoice =>possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceD.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+function generteComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    if(randomNumber === 1) {
+        computerchoice = 'rock'
+    }
+
+    if(randomNumber === 2) {
+        computerchoice = 'scissors'
+    }
+    
+    if(randomNumber === 3) {
+        computerchoice = 'paper'
+    }
+
+        computerChoiceD.innerHTML = computerchoice
+
+    
+    
+}
