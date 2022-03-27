22e4b5c5fcc319636d111b81664f71eeece4254a
Caleb <olupeniel2000@gmail.com>
2022-03-27 17:52:29 +0100

    updated rps

---
+++ portfolio/game.js
@@ -0,0 +1,11 @@
+const computerChoiceD = document.getElementById('computerchoice')
+const userChoiceD = document.getElementById("userchoice")
+const resultD = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice;
+
+possibleChoices.forEach(possibleChoice => button.addEventListener('click', (e) => {
+    userChoice = e.target.id
+
+    userChoiceD.innerHTML = userChoice
+}))
