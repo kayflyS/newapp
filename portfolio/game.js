3f16ec90e0b3cbbf22c55cad3c9161ec2c29fa7f
Caleb <olupeniel2000@gmail.com>
2022-03-30 19:10:13 +0100

    new working code

--- portfolio/game.js
+++ portfolio/game.js
@@ -1,32 +1,39 @@
+// using DOM to target html element 
 const computerChoiceD = document.getElementById('computerchoice')
 const userChoiceD = document.getElementById("userchoice")
 const resultD = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice;
 
-let computerchoice;
+let computerChoice;
 
+let result;
+// adding the click function
 possibleChoices.forEach(possibleChoice =>possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceD.innerHTML = userChoice
+   
     generateComputerChoice()
 }))
-
-/*function generteComputerChoice() {
+// function to generate commputer choice randomly
+function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1
-    if(randomNumber === 1) {
-        computerchoice = 'rock'
+    
+    if (randomNumber === 1) {
+        computerChoice = 'rock';
     }
 
-    if(randomNumber === 2) {
-        computerchoice = 'scissors'
+    if (randomNumber === 2) {
+        computerChoice = 'paper';
     }
     
-    if(randomNumber === 3) {
-        computerchoice = 'paper'
+    if (randomNumber === 3) {
+        computerChoice = 'scissors';
     }
 
-        computerChoiceD.innerHTML = computerchoice*/
-    
-    
+    computerChoiceD.innerHTML = computerChoice
 }
+// function for the result to access the winner
+
+
+
