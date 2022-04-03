aea3d80637ed8f8cd02d755fc9d2280b837e9371
Caleb <olupeniel2000@gmail.com>
2022-04-03 23:42:26 +0100

    test commit 7

--- Softdev/learn.js
+++ Softdev/learn.js
@@ -81,9 +81,14 @@ console.log(result);*/
 //     console.log(x);
 // }
  
-let a = [4,8,15,16,23,42];
-for (let i = 0; i < a.length; i++) {
+// let a = [4,8,15,16,23,42];
+// for (let i = 0; i < a.length; i++) {
     
-    console.log(a[i]);
-}
+//     console.log(a[i]);
+// }
 
+
+let x =1 
+while(x < 10){
+    console.log(x++);
+}
