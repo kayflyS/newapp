9235402101879fdbefef8f3097b59ae4556bf188
Caleb <olupeniel2000@gmail.com>
2022-04-07 23:46:34 +0100

    work3

--- Softdev/variables.js
+++ Softdev/variables.js
@@ -1,5 +1,5 @@
 function fortuneTeller(language, location, buy) {
 return `i can speak ${language} fluently and i also live in ${location} eating ${buy}`    
     }
-    let crystalBall = fortuneTeller("korea", "south korea","mango")
-    console.log(crystalBall);
+    let crystalBal = fortuneTeller("korea", "south korea","mango")
+    console.log(crystalBal);
