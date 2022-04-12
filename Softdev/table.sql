cd08d1adfe02f2aa63786641b0e6db5b737b3e1b
Caleb <olupeniel2000@gmail.com>
2022-04-12 23:04:39 +0100

    learning postgres

--- Softdev/table.sql
+++ Softdev/table.sql
@@ -7,5 +7,5 @@ CREATE TABLE book(
 );
 
 INSERT INTO book(id ,title,author,year)VALUES('1','Harry potter','j.k rowling','10-10-2001');
-
-
+INSERT INTO book(id ,title,author,year)VALUES('2','The diamond eye','kate quin','10-10-1997');
+INSERT INTO book(id ,title,author,year)VALUES('3','Midnight library','matt haig','10-10-1995');
