cdbba665b20a44f299ac7274f2a7c0a296d34aef
Caleb <olupeniel2000@gmail.com>
2022-04-19 08:56:27 +0100

    test commit

--- Softdev/table.sql
+++ Softdev/table.sql
@@ -9,4 +9,3 @@ CREATE TABLE book(
 INSERT INTO book(id ,title,author,year)VALUES('1','Harry potter','j.k rowling','10-10-2001');
 -- INSERT INTO book(id ,title,author,year)VALUES('2','The diamond eye','kate quin','25-06-1997');
 -- INSERT INTO book(id ,title,author,year)VALUES('3','Midnight library','matt haig','16-05-1995');
--- INSERT INTO book(id ,title,author,year)VALUES('4','French braid','anne tyler','4-08-2005');
--- Softdev/variables.js
+++ Softdev/variables.js
@@ -1,5 +1,5 @@
 function fortuneTeller(language, location, buy) {
 return `i can speak ${language} fluently and i also live in ${location} eating ${buy}`    
     }
-    let crystalBall = fortuneTeller("korea", "south korea","mango")
-    console.log(crystalBall);
+    let ball = fortuneTeller("korea", "south korea","mango")
+    console.log(ball);
