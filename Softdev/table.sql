82a019649ace13a0f7c4e6b8c2d1bf991bb63779
Caleb <olupeniel2000@gmail.com>
2022-04-21 23:42:05 +0100

    test commit

--- Softdev/table.sql
+++ Softdev/table.sql
@@ -1,10 +1,10 @@
--- CREATE TABLE book(
---     id SERIAL PRIMARY KEY,
---     title VARCHAR (50) NOT NULL,
---     author VARCHAR (100) NOT NULL,
---     year DATE,
---     created_on TIMESTAMP
--- );
+CREATE TABLE book(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR (50) NOT NULL,
+    author VARCHAR (100) NOT NULL,
+    year DATE,
+    created_on TIMESTAMP
+);
 
 INSERT INTO book(id ,title,author,year)VALUES('1','Harry potter','j.k rowling','10-10-2001');
 -- INSERT INTO book(id ,title,author,year)VALUES('2','The diamond eye','kate quin','25-06-1997');
