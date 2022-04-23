f9d65f2b1501d6f65bed7fdba27f170550ae9e15
Caleb <olupeniel2000@gmail.com>
2022-04-23 22:44:06 +0100

    learning js

--- Softdev/classwork.js
+++ Softdev/classwork.js
@@ -1,14 +1,14 @@
-// function printEvens(array) {
-//     let newArray =[]
-//     for(let x = 0; x < array.length;x++){
-//         for(let j = 0; j < array[j].length; j++){
-//             if(array[x][j] % 2 === 0){
-//                 newArray.push(array[x][j])
-//             }
-//         }
-//     }
-//     return newArray
-// }
+function printEvens(array) {
+    let newArray =[]
+    for(let x = 0; x < array.length;x++){
+        for(let j = 0; j < array[j].length; j++){
+            if(array[x][j] % 2 === 0){
+                newArray.push(array[x][j])
+            }
+        }
+    }
+    return newArray
+}
 
-//     const theArr = [[2,3],[4,5,6],[7,8],[9,10,11,12,13]]
+    const theArr = [[2,3],[4,5,6],[7,8],[9,10,11,12,13]]
     console.log(printEvens(theArr));
