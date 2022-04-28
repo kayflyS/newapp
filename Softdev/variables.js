9479fb094a754adfd9d4db807927862c47ff2c20
Caleb <olupeniel2000@gmail.com>
2022-04-28 23:55:20 +0100

    test commmit

--- Softdev/variables.js
+++ Softdev/variables.js
@@ -1,5 +1,5 @@
-// function fortuneTeller(language, location, buy) {
-// return `i can speak ${language} fluently and i also live in ${location} eating ${buy}`    
-//     }
-//     let ball = fortuneTeller("korean", "south korea","mango")
-//     console.log(ball);
+function fortuneTeller(language, location, buy) {
+return `i can speak ${language} fluently and i also live in ${location} eating ${buy}`    
+    }
+    let ball = fortuneTeller("korean", "south korea","mango")
+    console.log(ball);
