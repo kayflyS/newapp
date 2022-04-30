3a8e149cd1cd3c2b5519d203b932df4d50870e4b
Caleb <olupeniel2000@gmail.com>
2022-04-30 23:48:34 +0100

    test commit

--- Softdev/hacker.js
+++ Softdev/hacker.js
@@ -54,20 +54,20 @@ console.log(result);*/
 
 
 
-//    function vowelsAndConsonants(s) {
-//       const vowels = 'aeiou';
-//       var consonants = '';
+   function vowelsAndConsonants(s) {
+      const vowels = 'aeiou';
+      var consonants = '';
       
-//       for(var i = 0; i < s.length; i++) {
-//          if (vowels.includes(s[i])) {
-//              console.log(s[i]);
-//          }
-//          else {
-//              consonants += s[i] + '\n';
-//          }
-//       }
+      for(var i = 0; i < s.length; i++) {
+         if (vowels.includes(s[i])) {
+             console.log(s[i]);
+         }
+         else {
+             consonants += s[i] + '\n';
+         }
+      }
       
-//       console.log(consonants.trim());
-//   }
+      console.log(consonants.trim());
+  }
 
 
