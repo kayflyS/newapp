6b6d3e42ebf80773c7fa125d05ae0cd8fe692c1d
caleb olusegun <olupeniel2000@gmail.com>
2022-05-03 23:24:36 +0100

    test commit

--- Softdev/sololearn.js
+++ Softdev/sololearn.js
@@ -14,17 +14,17 @@
 //else {console.log("mango");
 
 //}
-// var day = 2;
-// switch (day) {
-//     case 1:
-//         document.write("Monday");
-//         break;
-//     case 2:
-//         document.write("Tuesday");
-//         break;
-//     case 3:
-//         document.write("Wednesday");
-//         break;
-//     default:
-//     console.log("Another day");
-// }
+var day = 2;
+switch (day) {
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("Wednesday");
+        break;
+    default:
+    console.log("Another day");
+}
