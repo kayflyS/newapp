7f46c60fb4075228c41089ccdc835887d4d7fa9e
caleb olusegun <olupeniel2000@gmail.com>
2022-06-23 23:36:24 +0100

    changes made

--- Softdev/sololearn.js
+++ Softdev/sololearn.js
@@ -1,12 +1,12 @@
 
-//function main() {
-    //var distance = parseInt(readLine(), 10);
-    //your code goes here
-  /* const speed = 40;
+function main() {
+    var distance = parseInt(readLine(), 10);
+    your code goes here
+  const speed = 40;
     const time = distance/speed;
     const timeInMinutes = time * 60;
     console.log(timeInMinutes);
-}*/
+}
 
 //let num1 = 7;
 //let num2 = 8;
