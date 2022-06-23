46638e642181e57b4997d392429588465e96b213
caleb olusegun <olupeniel2000@gmail.com>
2022-06-23 23:38:09 +0100

    changes made

--- Softdev/learn.js
+++ Softdev/learn.js
@@ -34,11 +34,11 @@ setTimeout(function () {
            console.log('FLOWERS');
        }
           
-       // Decision
+       Decision
 
-    //    let lap1 = 34
-    //    let lap2 = 33
-    //    let lap3 = 36
+       let lap1 = 34
+       let lap2 = 33
+       let lap3 = 36
        
        // Create a function that logs out the sum of all the lap times
       /*function add() {console.log(lap1 + lap2 + lap3);
