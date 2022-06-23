f067c83fee0d69d90ec03334ac8b25fb2c7efcd1
caleb olusegun <olupeniel2000@gmail.com>
2022-06-23 23:36:56 +0100

    changes made

--- Softdev/sololearn.js
+++ Softdev/sololearn.js
@@ -13,19 +13,19 @@ let num2 = 8;
 if (num1 > num2) {console.log//('WHAT THE FUCK'); }  
 else {console.log("mango");
 
-//}
-// var day = 2;
-// switch (day) {
-//     case 1:
-//         document.write("Monday");
-//         break;
-//     case 2:
-//         document.write("Tuesday");
-//         break;
-//     case 3:
-//         document.write("Wednesday");
-//         break;
-//     default:
-//     console.log("Another day");
-// }
+}
+var day = 2;
+switch (day) {
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("Wednesday");
+        break;
+    default:
+    console.log("Another day");
+}
 
