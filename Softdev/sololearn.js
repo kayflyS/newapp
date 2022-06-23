4342833a148061b8824a83c1dff95ab3dac94d11
caleb olusegun <olupeniel2000@gmail.com>
2022-06-23 23:36:38 +0100

    changes made

--- Softdev/sololearn.js
+++ Softdev/sololearn.js
@@ -8,10 +8,10 @@ function main() {
     console.log(timeInMinutes);
 }
 
-//let num1 = 7;
-//let num2 = 8;
-//if (num1 > num2) {console.log//('WHAT THE FUCK'); }  
-//else {console.log("mango");
+let num1 = 7;
+let num2 = 8;
+if (num1 > num2) {console.log//('WHAT THE FUCK'); }  
+else {console.log("mango");
 
 //}
 // var day = 2;
