27da07192c561989686c800cdfcda36e69df4b04
caleb olusegun <olupeniel2000@gmail.com>
2022-07-03 23:44:27 +0100

    changes made

--- Softdev/learn.js
+++ Softdev/learn.js
@@ -1,110 +1,110 @@
-// function myTax(amount) {
-//     let newAmount= amount * 2
-//     return newAmount;
+function myTax(amount) {
+    let newAmount= amount * 2
+    return newAmount;
 
     
-// }
-// let result = myTax(10)
-// console.log(result);
+}
+let result = myTax(10)
+console.log(result);
     
-// setTimeout(function () {
-//     console.log('now');
+setTimeout(function () {
+    console.log('now');
     
-//     },5000); 
+    },5000); 
 
-//     let counter = 0;
-//     function timeout() { 
-//         setTimeout(function () {
-//             console.log('HI ' + counter++);
+    let counter = 0;
+    function timeout() { 
+        setTimeout(function () {
+            console.log('HI ' + counter++);
 
             
-//         }, 2000)
-//     }
-//     timeout();
-//     // DECISION STATEMENTS
-//     if (6 > 10){
-//         console.log('BADDEST BOY');
-//     } else if
-//     (7 > 3){
-//         console.log('hello world');
-//     }
+        }, 2000)
+    }
+    timeout();
+    // DECISION STATEMENTS
+    if (6 > 10){
+        console.log('BADDEST BOY');
+    } else if
+    (7 > 3){
+        console.log('hello world');
+    }
 
     
-//         else{
-//            console.log('FLOWERS');
-//        }
+        else{
+           console.log('FLOWERS');
+       }
           
-//        Decision
+       Decision
 
-//        let lap1 = 34
-//        let lap2 = 33
-//        let lap3 = 36
+       let lap1 = 34
+       let lap2 = 33
+       let lap3 = 36
        
-//        // Create a function that logs out the sum of all the lap times
-//       /*function add() {console.log(lap1 + lap2 + lap3);
+       // Create a function that logs out the sum of all the lap times
+      /*function add() {console.log(lap1 + lap2 + lap3);
           
-//       }
-//       add();*/
+      }
+      add();*/
 
-//     //   let lapsCompleted = 0
+    //   let lapsCompleted = 0
 
-// // Create a function that increments the lapsCompleted variable with one
-// // Run it three times
-// // function increlap(){
-// //     lapsCompleted ++
-// // }
-// // increlap();
-// // increlap();
-// // increlap();
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+// function increlap(){
+//     lapsCompleted ++
+// }
+// increlap();
+// increlap();
+// increlap();
 
-// // console.log(lapsCompleted)
+// console.log(lapsCompleted)
 
-// // let hero = "batman"
-// // switch (hero) {
-// //     case "superman":
-// //         console.log("x-ray vision");
-// //         break;
-// //     case "batman":
-// //         console.log("fighting skills");
+// let hero = "batman"
+// switch (hero) {
+//     case "superman":
+//         console.log("x-ray vision");
+//         break;
+//     case "batman":
+//         console.log("fighting skills");
 
-// //         break;
-// //     default:
-// //         console.log("member of the  justice league"); 
-// // }
+//         break;
+//     default:
+//         console.log("member of the  justice league"); 
+// }
 
-// let a = 1, b = "1" 
-// let result = (a ==b ) ? 'equal' : 'inequal';
-// console.log(result);
+let a = 1, b = "1" 
+let result = (a ==b ) ? 'equal' : 'inequal';
+console.log(result);
 
-// for (let x = 0; x < 10; x++) {
+for (let x = 0; x < 10; x++) {
    
-//     console.log(x);
-// }
+    console.log(x);
+}
  
-// let a = [4,8,15,16,23,42];
-// for (let i = 0; i < a.length; i++) {
+let a = [4,8,15,16,23,42];
+for (let i = 0; i < a.length; i++) {
     
-//     console.log(a[i]);
-// }
+    console.log(a[i]);
+}
 
 
-// let x =1 
-// while(x < 10){
-//   console.log(++x);
+let x =1 
+while(x < 10){
+  console.log(++x);
 
-//     if(x==7)break;
+    if(x==7)break;
     
-// }
-// let a = 'first'
-// function scopeTest(){
+}
+let a = 'first'
+function scopeTest(){
 
-//     console.log(a);
-//     // let b = 'second'
-//     if(a!= ' '){
-//         console.log(a);
-//         let c = "third"
-//     }
-// }
+    console.log(a);
+    // let b = 'second'
+    if(a!= ' '){
+        console.log(a);
+        let c = "third"
+    }
+}
 // scopeTest()
 // console.log(b);
 // console.log(c);
