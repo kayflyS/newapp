d0725cfeb728f00af432ea32c8f847ed0384b2b6
caleb olusegun <olupeniel2000@gmail.com>
2022-07-06 23:44:10 +0100

    changes made

--- Softdev/calculator.js
+++ Softdev/calculator.js
@@ -12,23 +12,23 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 
 //division
-// const division = (num1, num2) => {
-//   if (num2 === 0) {
-//     return `Error enter denominator greater than 0`;
-//   }
-//   return num1 / num2;
-// };
+const division = (num1, num2) => {
+  if (num2 === 0) {
+    return `Error enter denominator greater than 0`;
+  }
+  return num1 / num2;
+};
 
 //exponential
 const exponential = (num1, num2)  => num1 ** num2
 
 const remainder = (num1, num2) => num1 % num2;
 
-// let resultAdd = calculator(10, 5, add);
-// console.log(resultAdd);
+let resultAdd = calculator(10, 5, add);
+console.log(resultAdd);
 
-// let resultSub = calculator(60, 40, subtract);
-// console.log(resultSub);
+let resultSub = calculator(60, 40, subtract);
+console.log(resultSub);
 
 let resultDiv = calculator(80, 4, division);
 console.log(resultDiv);
@@ -36,5 +36,5 @@ console.log(resultDiv);
 let resultRemainder = calculator(4, 3, remainder);
 console.log(resultRemainder);
 
-// let resultExpo = calculator(2, 1, exponential);
-// console.log(resultExpo);
+let resultExpo = calculator(2, 1, exponential);
+console.log(resultExpo);
