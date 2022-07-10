22664387e3cf629fd37c35737358190036871305
caleb olusegun <olupeniel2000@gmail.com>
2022-07-10 23:41:25 +0100

    changes made

--- Softdev/friend.js
+++ Softdev/friend.js
@@ -1,29 +1,29 @@
-var friend = {
-    firstName: "Ash" ,
-    sayHi:
-    function() {
-        return this.firstName+ "says hello!";
+// var friend = {
+//     firstName: "Ash" ,
+//     sayHi:
+//     function() {
+//         return this.firstName+ "says hello!";
         
-    }
-};
-friend.sayHi()
+//     }
+// };
+// friend.sayHi()
 
-var dog = {
-    name: "whiskey",
-    sleep:function(){
-        return this.name + "is sleeping: zzzzz..."
-    }
-}
+// var dog = {
+//     name: "whiskey",
+//     sleep:function(){
+//         return this.name + "is sleeping: zzzzz..."
+//     }
+// }
 
-dog.sleep();
+// dog.sleep();
 
-var nested = {
-    number: 1,
-    anotherObject: {
-        anotherNumber: 2,
-        whatIsThis:function(){
-            return this 
-        }
-    }
-}
-nested.anotherObject.whatIsThis();
+// var nested = {
+//     number: 1,
+//     anotherObject: {
+//         anotherNumber: 2,
+//         whatIsThis:function(){
+//             return this 
+//         }
+//     }
+// }
+// nested.anotherObject.whatIsThis();
