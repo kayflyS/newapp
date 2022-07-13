8ccab63bc9eb49799faf80ec72221a3b1f2f0589
caleb olusegun <olupeniel2000@gmail.com>
2022-07-13 23:50:19 +0100

    changes made

--- Softdev/reverse.js
+++ Softdev/reverse.js
@@ -1,11 +1,11 @@
   //Reverse a string without js function
-  function reverse(str){
-    var r = "";
-    for(var i = str.length - 1; i >= 0; i--){
-      r += str[i]
-    }
-    return r;
-  }
+  // function reverse(str){
+  //   var r = "";
+  //   for(var i = str.length - 1; i >= 0; i--){
+  //     r += str[i]
+  //   }
+  //   return r;
+  // }
   //Recursion
   // function countDown(num) {
   //   console.log(num);
