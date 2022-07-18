d4e3a9b6eef9db40aecac5aedf4c4ea24ddeb2b8
caleb olusegun <olupeniel2000@gmail.com>
2022-07-18 23:40:28 +0100

    changes made

--- Softdev/hacker.js
+++ Softdev/hacker.js
@@ -9,14 +9,14 @@
 // let result = getArea(3,4.5)
 // console.log(result);
 
-function getPerimeter(length, width) {
-   let perimeter;
-   // Write your code here
-   perimeter =  2 * (length + width)
-   return perimeter;
-}
- let answer = getPerimeter(3,4.5)
-console.log(answer);
+// function getPerimeter(length, width) {
+//    let perimeter;
+//    // Write your code here
+//    perimeter =  2 * (length + width)
+//    return perimeter;
+// }
+//  let answer = getPerimeter(3,4.5)
+// console.log(answer);
 
 const firstInterger = 5
 const firstDecimal = 5.8
