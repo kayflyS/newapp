c612e37ceb223c1ac9be933a67f23d1ef02782f2
caleb olusegun <olupeniel2000@gmail.com>
2022-07-25 23:56:15 +0100

    changes made

--- Softdev/table.sql
+++ Softdev/table.sql
@@ -1,9 +1,9 @@
--- CREATE TABLE book(
---     id SERIAL PRIMARY KEY,
---     title VARCHAR (50) NOT NULL,
---     author VARCHAR (100) NOT NULL,
---     year DATE,
---     created_on TIMESTAMP
--- );
+CREATE TABLE book(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR (50) NOT NULL,
+    author VARCHAR (100) NOT NULL,
+    year DATE,
+    created_on TIMESTAMP
+);
 
--- INSERT INTO book(id ,title,author,year)VALUES('1','Harry potter','j.k rowling','10-10-2001');
+INSERT INTO book(id ,title,author,year)VALUES('1','Harry potter','j.k rowling','10-10-2001');
