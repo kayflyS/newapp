f2d6e6aad0783e1ded3314b5334cab96e2ddd2a4
caleb olusegun <olupeniel2000@gmail.com>
2022-07-29 23:53:23 +0100

    changes made

--- Softdev/variables.js
+++ Softdev/variables.js
@@ -1,5 +1,5 @@
-// function fortuneTeller(language, location, buy) {
-// return `i can speak ${language} fluently and i also live in ${location} eating ${buy}`    
-//     }
-//     let crystalBall = fortuneTeller("korean", "south korea","mango")
-//     console.log(crystalBall);
+function fortuneTeller(language, location, buy) {
+return `i can speak ${language} fluently and i also live in ${location} eating ${buy}`    
+    }
+    let crystalBall = fortuneTeller("korean", "south korea","mango")
+    console.log(crystalBall);
