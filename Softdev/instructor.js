6211f970e53c86be52882e92a2709c4a1e07fafa
caleb olusegun <olupeniel2000@gmail.com>
2022-07-30 23:56:54 +0100

    changes made

--- Softdev/instructor.js
+++ Softdev/instructor.js
@@ -1,14 +1,14 @@
-// var instructor = {
-//     firstName:'Tim',
-//     info: {
-//         catOwner: true,
-//         boatOwner: true,
-//         displaylocation:function() {
-//             return this.displaylocation.location;  
-//         },
-//         data: {
-//             location: "Oakland"
-//         }
-//     }
-// }
-// instructor.info.displaylocation()              
+var instructor = {
+    firstName:'Tim',
+    info: {
+        catOwner: true,
+        boatOwner: true,
+        displaylocation:function() {
+            return this.displaylocation.location;  
+        },
+        data: {
+        }
+    }
+}
+instructor.info.displaylocation()                    location: "Oakland"
+      
