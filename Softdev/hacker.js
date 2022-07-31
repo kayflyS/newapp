6091e9829a990994641902200c96c2086b7cc251
caleb olusegun <olupeniel2000@gmail.com>
2022-07-31 23:50:20 +0100

    changes made

--- Softdev/hacker.js
+++ Softdev/hacker.js
@@ -18,56 +18,56 @@
 //  let answer = getPerimeter(3,4.5)
 // console.log(answer);
 
-const firstInterger = 5
-const firstDecimal = 5.8
-const firstString = "hacker"
+// const firstInterger = 5
+// const firstDecimal = 5.8
+// const firstString = "hacker"
 
-const secondInterger = 7
-const secondDecimal = 10.8
-const secondString = "rank is easy"
+// const secondInterger = 7
+// const secondDecimal = 10.8
+// const secondString = "rank is easy"
 
-console.log(firstInterger + secondInterger);
+// console.log(firstInterger + secondInterger);
 
-console.log(firstDecimal + secondDecimal);
+// console.log(firstDecimal + secondDecimal);
 
-console.log(firstString + secondString);
+// console.log(firstString + secondString);
 
-function factorial(n){
-   if(n === 0){
-       return 1;
-   } else{
-       return n * factorial(n-1);
-   }
-}
-let result = factorial(4)
-console.log(result);
+// function factorial(n){
+//    if(n === 0){
+//        return 1;
+//    } else{
+//        return n * factorial(n-1);
+//    }
+// }
+// let result = factorial(4)
+// console.log(result);
 
-function factorial(n) {
-   let result = 1;
-   for (let j = n; j > 1; j--){
-      result *= j;
-   }
-   return result;
-   }
-   let answer = factorial(4)
-   console.log(answer);
+// function factorial(n) {
+//    let result = 1;
+//    for (let j = n; j > 1; j--){
+//       result *= j;
+//    }
+//    return result;
+//    }
+//    let answer = factorial(4)
+//    console.log(answer);
 
 
 
-   function vowelsAndConsonants(s) {
-      const vowels = 'aeiou';
-      var consonants = '';
+//    function vowelsAndConsonants(s) {
+//       const vowels = 'aeiou';
+//       var consonants = '';
       
-      for(var i = 0; i < s.length; i++) {
-         if (vowels.includes(s[i])) {
-             console.log(s[i]);
-         }
-         else {
-             consonants += s[i] + '\n';
-         }
-      }
+//       for(var i = 0; i < s.length; i++) {
+//          if (vowels.includes(s[i])) {
+//              console.log(s[i]);
+//          }
+//          else {
+//              consonants += s[i] + '\n';
+//          }
+//       }
       
-      console.log(consonants.trim());
-  }
+//       console.log(consonants.trim());
+//   }
 
 
