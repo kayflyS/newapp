122c0fb5a16335dcad72c5cd87ef5e4d2f0ae3ce
caleb olusegun <olupeniel2000@gmail.com>
2022-08-01 22:18:37 +0100

    changes made

--- Softdev/learn.js
+++ Softdev/learn.js
@@ -7,40 +7,40 @@
 // let result = myTax(10)
 // console.log(result);
     
-setTimeout(function () {
-    console.log('now');
+// setTimeout(function () {
+//     console.log('now');
     
-    },5000); 
+//     },5000); 
 
-    let counter = 0;
-    function timeout() { 
-        setTimeout(function () {
-            console.log('HI ' + counter++);
+//     let counter = 0;
+//     function timeout() { 
+//         setTimeout(function () {
+//             console.log('HI ' + counter++);
 
             
-        }, 2000)
-    }
-    timeout();
-
-
-//     DECISION STATEMENTS
-//     if (6 > 10){
-//         console.log('BADDEST BOY');
-//     } else if
-//     (7 > 3){
-//         console.log('hello world');
+//         }, 2000)
 //     }
+//     timeout();
+
+
+    DECISION STATEMENTS
+    if (6 > 10){
+        console.log('BADDEST BOY');
+    } else if
+    (7 > 3){
+        console.log('hello world');
+    }
 
     
-//         else{
-//            console.log('FLOWERS');
-//        }
+        else{
+           console.log('FLOWERS');
+       }
           
-//        Decision
+       Decision
 
-//        let lap1 = 34
-//        let lap2 = 33
-//        let lap3 = 36
+       let lap1 = 34
+       let lap2 = 33
+       let lap3 = 36
        
 //        // Create a function that logs out the sum of all the lap times
 //       function add() {console.log(lap1 + lap2 + lap3);
