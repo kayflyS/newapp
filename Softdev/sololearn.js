b2e0f8c412a20a649539c54fc96ae8d8c47bf6e5
caleb olusegun <olupeniel2000@gmail.com>
2022-08-03 23:50:14 +0100

    changes made

--- Softdev/sololearn.js
+++ Softdev/sololearn.js
@@ -1,31 +1,31 @@
 
-function main() {
-    var distance = parseInt(readLine(), 10);
-    // your code goes here
-  const speed = 40;
-    const time = distance/speed;
-    const timeInMinutes = time * 60;
-    console.log(timeInMinutes);
-}
+// function main() {
+//     var distance = parseInt(readLine(), 10);
+//     // your code goes here
+//   const speed = 40;
+//     const time = distance/speed;
+//     const timeInMinutes = time * 60;
+//     console.log(timeInMinutes);
+// }
 
 // let num1 = 7;
-let num2 = 8;
-if (num1 > num2) {console.log//('WHAT THE FUCK'); }  
-else {console.log("mango");
+// let num2 = 8;
+// if (num1 > num2) {console.log//('WHAT THE FUCK'); }  
+// else {console.log("mango");
 
-}
-var day = 2;
-switch (day) {
-    case 1:
-        document.write("Monday");
-        break;
-    case 2:
-        document.write("Tuesday");
-        break;
-    case 3:
-        document.write("Wednesday");
-        break;
-    default:
-    console.log("Another day");
-}
+// }
+// var day = 2;
+// switch (day) {
+//     case 1:
+//         document.write("Monday");
+//         break;
+//     case 2:
+//         document.write("Tuesday");
+//         break;
+//     case 3:
+//         document.write("Wednesday");
+//         break;
+//     default:
+//     console.log("Another day");
+// }
 
