e909331fbfe74ef231aae00a51675783832e79b3
caleb olusegun <olupeniel2000@gmail.com>
2022-08-20 23:07:46 +0100

    changes made

--- Softdev/calculator.js
+++ Softdev/calculator.js
@@ -1,40 +1,40 @@
-const calculator = (num1, num2, operation) => {
-  if (operation instanceof Function) {
-    return operation(num1, num2);
-  }
-  return;
-};
+// const calculator = (num1, num2, operation) => {
+//   if (operation instanceof Function) {
+//     return operation(num1, num2);
+//   }
+//   return;
+// };
 
-// addition
-const add = (num1, num2) => num1 + num2;
+// // addition
+// const add = (num1, num2) => num1 + num2;
 
-// substraction
-const subtract = (num1, num2) => num1 - num2;
+// // substraction
+// const subtract = (num1, num2) => num1 - num2;
 
-//division
-const division = (num1, num2) => {
-  if (num2 === 0) {
-    return `Error enter denominator greater than 0`;
-  }
-  return num1 / num2;
-};
+// //division
+// const division = (num1, num2) => {
+//   if (num2 === 0) {
+//     return `Error enter denominator greater than 0`;
+//   }
+//   return num1 / num2;
+// };
 
-//exponential
-const exponential = (num1, num2)  => num1 ** num2
+// //exponential
+// const exponential = (num1, num2)  => num1 ** num2
 
-const remainder = (num1, num2) => num1 % num2;
+// const remainder = (num1, num2) => num1 % num2;
 
-let resultAdd = calculator(100, 50, add);
-console.log(resultAdd);
+// let resultAdd = calculator(100, 50, add);
+// console.log(resultAdd);
 
-let resultSub = calculator(6, 4, subtract);
-console.log(resultSub);
+// let resultSub = calculator(6, 4, subtract);
+// console.log(resultSub);
 
-let resultDiv = calculator(80, 4, division);
-console.log(resultDiv);
+// let resultDiv = calculator(80, 4, division);
+// console.log(resultDiv);
 
-let resultRemainder = calculator(4, 3, remainder);
-console.log(resultRemainder);
+// let resultRemainder = calculator(4, 3, remainder);
+// console.log(resultRemainder);
 
-let resultExpo = calculator(2, 1, exponential);
-console.log(resultExpo);
+// let resultExpo = calculator(2, 1, exponential);
+// console.log(resultExpo);
