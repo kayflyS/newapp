de3a5626533c56399b412de5a864b26c23f9512c
caleb olusegun <olupeniel2000@gmail.com>
2022-09-01 00:00:58 +0100

    changes made

--- Softdev/gorm.js
+++ Softdev/gorm.js
@@ -1,19 +1,19 @@
-function factorial(n){
-    if(n === 0){
-        return 1;
-    }else{
-        return n * factorial(n-1);
-    }
-}
-let result = factorial(4)
-console.log(result);
-const factorialOf = integer => {
-    let factorial = 1;
+// function factorial(n){
+//     if(n === 0){
+//         return 1;
+//     }else{
+//         return n * factorial(n-1);
+//     }
+// }
+// let result = factorial(4)
+// console.log(result);
+// const factorialOf = integer => {
+//     let factorial = 1;
   
-    for(let i = 1; i <= integer; i++) {
-      factorial *= i;
-    }
+//     for(let i = 1; i <= integer; i++) {
+//       factorial *= i;
+//     }
   
-    return factorial;
-  } 
+//     return factorial;
+//   } 
   
